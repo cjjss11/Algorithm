2c6322991d357961278b242fa6da722d1ab3bf0b
최지수 <cjjss11@naver.com>
2023-04-13 00:00:36 +0900

    1929. 소수 구하기

--- SILVER/1929-소수구하기.py
+++ SILVER/1929-소수구하기.py
@@ -2,7 +2,7 @@ M,N = map(int,input().split())
 lst = [1]*(N+1)
 lst[0] = 0
 lst[1] = 0
-for i in range(2,int(N**0.5)+1): # 제곱근을 사용해서 해당 제곱근 값으로 범위지정
+for i in range(2,int(N**0.5)+1): # 제곱근을 사용해서 해당 제곱근 값으로 범위 지정
     if lst[i] == 1:
         for j in range(i*2, N+1, i):
             lst[j] = 0
