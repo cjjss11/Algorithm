4c5ad663fe09f02e6bd2a13017d0ba2fb6eec9d9
최지수 <cjjss11@naver.com>
2023-06-20 20:07:09 +0900

    14284. 간선 이어가기2

---
+++ GOLD/14284-간선이어가기2.py
@@ -0,0 +1,33 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+lst = [[] for _ in range(N+1)]
+INF = int(1e9)
+distance = [INF] * (N+1)
+
+for _ in range(M):
+    a,b,c = map(int,input().split())
+    lst[a].append((b,c))
+    lst[b].append((a,c))
+
+start,end = map(int,input().split())
+
+def dijkstra(start):
+    distance[start] = 0
+
+    q = []
+    heapq.heappush(q,(0,start))
+    while q:
+        dist,now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in lst[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q,(cost,i[0]))
+
+dijkstra(start)
+print(distance[end])
