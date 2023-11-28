a619eca63e8632b0fc965c289c65079cd991b39c
최지수 <cjjss11@naver.com>
2023-11-28 11:24:38 +0900

    개인정보 수집 유효기간 주석 추가

--- ALGORITHM/개인정보 수집 유효기간.py
+++ ALGORITHM/개인정보 수집 유효기간.py
@@ -1,10 +1,12 @@
 def solution(today, terms, privacies):
     answer = []
+    # 오늘의 날짜를 연도, 월, 일로 나누고 int로 변환
     today_year = int(today[0]+today[1]+today[2]+today[3])
     today_month = int(today[5]+today[6])
     today_day = int(today[8]+today[9])
-    terms_dic = {}
     
+    # 유효기간 담을 딕셔너리
+    terms_dic = {}
     for i in terms:
         grade,month = i.split()
         terms_dic[grade] = int(month)
@@ -15,15 +17,22 @@ def solution(today, terms, privacies):
         month = int(date[5] + date[6])
         day = int(date[8] + date[9])
         
+        # 개인정보 수집 일자에서 월에다가 유효기간을 더함
         month = month + terms_dic[grade]
+        # 더했을 때 12배수이면
         if month % 12 == 0:
+            # ex) 개인정보 수집 일자 11월이고 유효기간이 13달일 때 더하면 24이므로 
+            # 연도가 1 플러스 되고 달은 12월로 되어야 함 
             year = year + ((month//12)-1)
             month = 12
+        # 더했을 때 12배수가 아니면
         else:
             if month > 12:
                 year = year + (month//12)
                 month = month % 12
-            
+        
+        # 오늘 날짜랑 개인정보 수집 일자를 각각 연도, 월, 일 비교해서 
+        # 오늘 날짜가 더 크면 answer에 담기
         if today_year > year:
             answer.append(i+1)
         elif today_year == year:
