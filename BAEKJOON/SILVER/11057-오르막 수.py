3c949bec8eb9257ff16229f64ceb5fe19be6d177
최지수 <cjjss11@naver.com>
2024-02-03 17:22:30 +0900

    11057-오르막 수 두 번째 방법 문제 풀이

--- BAEKJOON/SILVER/11057-오르막 수.py
+++ BAEKJOON/SILVER/11057-오르막 수.py
@@ -1,4 +1,7 @@
+# 첫 번째 방법 / 뒤 쪽에 숫자 추가
+
 N = int(input())
+# dp를 2차 리스트로 초기화
 dp = [[0]*10 for _ in range(N+1)]
 dp[1] = [1]*10
 
@@ -7,3 +10,13 @@ for i in range(2, N+1):
         dp[i][j] = sum(dp[i-1][j:])
 
 print(sum(dp[N]) % 10007)
+
+# 두 번째 방법 / 앞 쪽에 숫자 추가
+N = int(input())
+# dp를 1차 리스트에 1로 초기화
+dp = [1]*10
+
+for i in range(N-1):
+    for j in range(1, 10):
+        dp[j] = dp[j] + dp[j-1]
+print(sum(dp) % 10007)
