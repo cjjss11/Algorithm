cbf4fe7312238093d063366e8c4ca8fc85f8f170
최지수 <cjjss11@naver.com>
2024-03-27 23:38:14 +0900

    피자 나눠 먹기(2) 문제 다른 풀이

--- PROGRAMMERS/ALGORITHM/피자 나눠 먹기(2).java
+++ PROGRAMMERS/ALGORITHM/피자 나눠 먹기(2).java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1000;
-        int pizza = 0;
+        int answer = 0;
         for (int i=1; i<=6; i++) {
             int num = n * i;
             if (num % 6 == 0) {
-                pizza = num / 6;
-                if (pizza < answer) {
-                    answer = pizza;
-                }
+                answer = num / 6;
+                break;
             }
         }
         return answer;
