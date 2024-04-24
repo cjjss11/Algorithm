79134348bb797b4b1a7eea882f07fe1d00c8987c
최지수 <cjjss11@naver.com>
2024-04-24 14:48:36 +0900

    구슬을 나누는 경우의 수 문제 자바 언어로 풀이

---
+++ PROGRAMMERS/ALGORITHM/구슬을 나누는 경우의 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int balls, int share) {
+        int answer = 0;
+        answer = combination(balls, share);
+        return answer;
+    }
+    public static int combination(int balls, int share) {
+        if (balls == share || share == 0) {
+            return 1;
+        } 
+        return combination((balls -1), (share -1)) + combination(balls -1, share);
+    }
+}
