48e9f7cfed57fed1c817c9cdae3436dab53c7f0b
최지수 <cjjss11@naver.com>
2024-04-24 14:47:51 +0900

    단어 변환 문제 다른 방법으로 풀이

--- PROGRAMMERS/ALGORITHM/단어 변환.py
+++ PROGRAMMERS/ALGORITHM/단어 변환.py
@@ -1,3 +1,4 @@
+# 첫 번째 방법 dfs 풀이
 def solution(begin, target, words):
     global answer,result
     
@@ -25,4 +26,29 @@ def solution(begin, target, words):
                 used[i] = 0
                 result -= 1
     dfs(begin)
+    return answer
+
+# 두 번째 방법 bfs 풀이
+from collections import deque
+
+def solution(begin, target, words):
+    answer = 0
+    q = deque()
+    q.append((begin, 0))
+    used = [0] * len(words)
+    
+    while q:
+        word, cnt = q.popleft()
+        if word == target:
+            answer = cnt
+            break
+        for i in range(len(words)):
+            word_cnt = 0
+            if used[i] == 0:
+                for j in range(len(word)):
+                    if words[i][j] != word[j]:
+                        word_cnt += 1
+                if word_cnt == 1:
+                    q.append((words[i], cnt+1))
+                    used[i] = 1
     return answer
